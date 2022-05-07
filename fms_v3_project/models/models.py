15f3e40a410519e8099d852434d14e2396906e29
Zhenya1975 <zhenya1975@mail.ru>
2022-05-07 17:53:03 +0300

    dded column delete status

--- fms_v3_project/models/models.py
+++ fms_v3_project/models/models.py
@@ -8,5 +8,16 @@ class CompetitionsDB(db.Model):
     competition_date_start = db.Column(db.Date, default=datetime.utcnow)
     competition_date_finish = db.Column(db.Date, default=datetime.utcnow)
     competition_city = db.Column(db.String)
+    delete_status = db.Column(db.Boolean, default=False)
+    registrations = db.relationship('RegistrationDB', backref='competition')
     # competition_fights = db.relationship('FightsDB', backref='competition', lazy='dynamic')
     # registrations = db.relationship('RegistrationDB', backref='competition')
+"""Модель для регистрации"""
+class RegistrationDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    competition_id = db.Column(db.Integer, db.ForeignKey('competitionsDB.competition_id'))
+    # fighter_id = db.Column(db.Integer, db.ForeignKey('fightersDB.fighter_id'))
+    fighter_registration_weight = db.Column(db.Integer)
+    fighter_registration_age = db.Column(db.Integer)
+    # weight_cat_id = db.Column(db.Integer, db.ForeignKey('weightcategoriesDB.weight_cat_id'))
+    # age_cat_id = db.Column(db.Integer, db.ForeignKey('agecategoriesDB.id'))
